0b9c1e4a7f390d7a246debb2457620f9b1cff662
Jay T <torrcorp@gmail.com>
2018-06-07 10:25:07 -0600

    Use input from client

--- src/edu/cnm/deepdive/codebreaker/Client.java
+++ src/edu/cnm/deepdive/codebreaker/Client.java
@@ -1,11 +1,10 @@
 package edu.cnm.deepdive.codebreaker;
 
 import edu.cnm.deepdive.codebreaker.models.Game;
+import edu.cnm.deepdive.codebreaker.models.Guess;
 import edu.cnm.deepdive.codebreaker.services.GameService;
-import java.util.List;
-import java.util.UUID;
-import retrofit2.Call;
-import retrofit2.Callback;
+import java.io.IOException;
+import java.util.Scanner;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -19,8 +18,37 @@ public class Client {
         .build();
 
     GameService gameService = retrofit.create(GameService.class);
+    Scanner scanner = new Scanner(System.in);
+    Game game;
+    try {
+      Game gameSpecification = new Game();
+      System.out.println("Enter the characters to use in this game:");
+      String characters = scanner.nextLine();
+      gameSpecification.setCharacters(characters);
+      game = gameService.createGame(gameSpecification).execute().body();
+    } catch (IOException e) {
+      throw new RuntimeException("Error creating game", e);
+    }
+    while (true) {
+      System.out.printf("Enter a Guess of %d characters:", game.getLength());
 
-    gameService.findById(UUID.fromString("237895c4-0a9e-4ebf-8162-8ae9afdad4b2")).enqueue(
+      String guessString = scanner.nextLine().trim();
+      Guess guess = new Guess();
+      guess.setGuess(guessString);
+      try {
+        Response<Guess> guessResponse = gameService.createGuess(game.getId(), guess)
+            .execute();
+
+        Guess guessFromServer = guessResponse.body();
+        System.out.printf("In Place: %d Out of Place: %d \n",
+            guessFromServer.getInPlace(),
+            guessFromServer.getOutOfPlace());
+      } catch (IOException e) {
+        throw new RuntimeException("Error Sending Guess", e);
+      }
+    }
+
+   /* gameService.findById(UUID.fromString("237895c4-0a9e-4ebf-8162-8ae9afdad4b2")).enqueue(
           new Callback<Game>() {
       @Override
       public void onResponse(Call<Game> call, Response<Game> response) {
@@ -33,6 +61,7 @@ public class Client {
       public void onFailure(Call<Game> call, Throwable throwable) {
           throw new RuntimeException("Request failed.", throwable);
       }
-    });
+    });*/
+
   }
 }
